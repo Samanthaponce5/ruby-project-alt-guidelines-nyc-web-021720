8a1e6093e7fa7280a9a0e4722acbd6bee75f8710
github username <github email address>
2020-03-04 16:36:47 -0500

    get_destination_and_price

--- bin/run.rb
+++ bin/run.rb
@@ -4,3 +4,5 @@ require_relative '../config/environment'
 cli = CommandLineInterface.new
 cli.greet 
 cli.get_tourist
+cli.get_destination
+
--- lib/cli2.rb
+++ lib/cli2.rb
@@ -22,4 +22,14 @@ class CommandLineInterface
         age_input = gets.chomp.to_i 
         create_tourist(name_input, age_input)
     end 
+
+    def get_destination
+        puts "Please enter the city you'd like to visit:"
+        city_input = gets.chomp 
+        puts "Please enter the country:"
+        country_input = gets.chomp 
+        destination = to_destination(city_input, country_input)
+        price = rand(350...800)
+        puts "Going to #{city_input}, #{country_input} will cost $#{price}"
+    end 
 end 
